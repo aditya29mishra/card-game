2fceccd5614dbc36bd7615a710f10f04caa73bf0
aditya29mishra <adityarakeshmishra@gmail.com>
2025-09-15 22:16:27 +0530

    bug

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -320,7 +320,7 @@ public class GameManager : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-            SaveGame()
+            SaveGame();
             Application.Quit();
 #endif
     }
